9e9212277744b8fa091c43edee3e07ac3443a97d
Largenty <Largenty@gmail.com>
2020-04-10 14:52:36 +0200

    add HTML footer

--- front-end/src/components/Footer/index.js
+++ front-end/src/components/Footer/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import './style.scss';
 
 const Footer = () => (
-  <h1>Footer</h1>
+  <div className="Footer__container">
+    <a className="Footer__link" href="#">Mentions légales</a>
+    <a className="Footer__link" href="#">Contacter le support</a>
+    <a className="Footer__link" href="#">Informations</a>
+    <a className="Footer__link"href="#">FAQ</a>
+  </div>
 )
 ;
 
