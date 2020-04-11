b533e7bd45839210ea54dcc0caec420813547922
charlie <charlie.josuas@gmail.com>
2020-04-11 16:54:40 +0200

    layout home +responsiv

---
+++ front-end/src/components/Home/HomeProfile.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './style.scss';
+
+const HomeProfile = () => (
+  <>
+    <img scr="" alt="" />
+    <img scr="" alt="" />
+    <p>Prénom NOM</p>
+    <p>Role</p>
+    <p>Voir mon profil</p>
+    <p>Modifier mon profil</p>
+    <p>Mes annonces</p>
+    <p>Mes messages</p>
+    <p>Paramètres</p>
+    <p>Deconnexion</p>
+    <input type="button" value="Poster une annonce" />
+  </>
+)
+;
+
+export default HomeProfile;
--- front-end/src/components/Home/index.js
+++ front-end/src/components/Home/index.js
@@ -1,8 +1,67 @@
 import React from 'react';
 import './style.scss';
+import Profile from './HomeProfile';
 
 const Home = () => (
-  <h1>Home</h1>
+  <div className="home__container">
+    <div className="home__left">
+      <div className="home__profile">
+        <Profile />
+      </div>
+      <div className="home__news"/>
+    </div>
+    <div className="home__announcementlist">
+      <ul className="home__navlink">
+        <li>Dernieres annonces</li>
+        <li>Mes favoris</li>
+        <li>Rehercher une annonce</li>
+      </ul>
+
+      <div className="home__announcement">
+        <div className="home__announcement__img">
+          <img src="" alt="announcement"/>
+        </div>
+        <div className="home__announcement__avatar">
+          <img src="" alt="avatar"/>
+        </div>
+        <p>Nom du posteur</p>
+        <p>Role du posteur</p>
+        <p>Titre de l'annonce</p>
+        <p>Description de l'annonce</p>
+        <p>Date de création de l'annonce</p>
+        <input type="button" value="Voir l'annonce"/>
+      </div>
+      <div className="home__announcement">
+        <div className="home__announcement__img">
+          <img src="" alt="announcement"/>
+        </div>
+        <div className="home__announcement__avatar">
+          <img src="" alt="avatar"/>
+        </div>
+        <p>Nom du posteur</p>
+        <p>Role du posteur</p>
+        <p>Titre de l'annonce</p>
+        <p>Description de l'annonce</p>
+        <p>Date de création de l'annonce</p>
+        <input type="button" value="Voir l'annonce"/>
+      </div>
+      <div className="home__announcement">
+        <div className="home__announcement__img">
+          <img src="" alt="announcement"/>
+        </div>
+        <div className="home__announcement__avatar">
+          <img src="" alt="avatar"/>
+        </div>
+        <p>Nom du posteur</p>
+        <p>Role du posteur</p>
+        <p>Titre de l'annonce</p>
+        <p>Description de l'annonce</p>
+        <p>Date de création de l'annonce</p>
+        <input type="button" value="Voir l'annonce"/>
+      </div>
+
+    </div>
+  </div>
 )
 ;
 
