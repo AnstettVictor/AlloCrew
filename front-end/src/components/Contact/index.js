5b2a24182904786d99b58dc52832389ceaf2e627
Largenty <Largenty@gmail.com>
2020-04-11 18:37:15 +0200

    created HTML/CSS for contact

--- front-end/src/components/Contact/index.js
+++ front-end/src/components/Contact/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './style.scss';
 
+
 const Contact = () => (
-  <h1>Contact</h1>
-)h
+  <div className="contact__container">
+    <h3 className="contact__title">Contacter le support.</h3>
+    <p className="contact__text">Si vous rencontrez des difficultés ou si vous avez des questions à nous poser, n'hésitez pas à nous écrire un petit message à l'adresse suivante :
+    support@allocrew.com</p>
+    
+  </div>
+)
 ;
 
 export default Contact;
