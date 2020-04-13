9381d14488b87b48c11b83bd6992697727795f7e
charlie <charlie.josuas@gmail.com>
2020-04-13 14:30:29 +0200

    update font

--- front-end/src/components/App/index.js
+++ front-end/src/components/App/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './style.scss';
-import Home from '../Home';
+import Header from '../Header';
 
 const App = () => (
   <>
     <div className="app__header" >
+      <Header />
     </ div>
     <div className="app__main">
     </div>
