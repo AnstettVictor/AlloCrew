b7bb083756667905648d2aa3aaf06e42444c3be4
charlie <charlie.josuas@gmail.com>
2020-04-13 23:42:10 +0200

    datepicker install

--- front-end/src/components/App/index.js
+++ front-end/src/components/App/index.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style.scss';
 import Header from '../Header';
 import Footer from '../Footer';
 import Home from '../Home';
+import DatePicker from 'react-datepicker'
 
 
-const App = () => (
+
+const App = () => {
+
+const [startDate, setStartDate] = useState(new Date());
+return(
   <>
     <div className="app__header" >
       <Header />
     </ div>
     <div className="app__main">
-      <Home />
+      <DatePicker className='input' selected={startDate} />
     </div>
     <div className="app__footer">
       <Footer />
     </div>
   </>
 )
+}
 ;
 
 export default App;
