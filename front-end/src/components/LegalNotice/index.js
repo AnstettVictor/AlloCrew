8441ce379140b50bcba402f5653b8a6c87078388
bogdanpatroi <bujica.bogdan@gmail.com>
2020-04-14 00:09:31 +0200

    LegalNotice component done, (html + CSS)

--- front-end/src/components/LegalNotice/index.js
+++ front-end/src/components/LegalNotice/index.js
@@ -2,8 +2,45 @@ import React from 'react';
 import './style.scss';
 
 const LegalNotice = () => (
-  <h1>LegalNoticeee</h1>
-)
-;
+  <div className="legalNotice__container">
+    <h1 className="legalNotice__titlePage">Mentions légales</h1>
+    <article className="legalNotice__article">
+      <h2 className="legalNotice__titleArticle">Phasellus non sodales mi</h2>
+      <p className="legalNotice__pArticle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia augue libero, eget vehicula magna pharetra at. Morbi fringilla diam erat, non porttitor elit euismod ut. Sed ac rutrum metus. Integer vel efficitur orci. Aliquam nec cursus urna. Sed vitae maximus velit, id molestie ipsum. Sed a nibh sed felis ultrices tempor commodo ut sapien. Morbi suscipit ex justo, vitae faucibus sem scelerisque vel. Nulla vitae felis ac tortor venenatis efficitur ac sit amet lectus. Vestibulum dapibus ac risus sit amet gravida. Pellentesque porttitor quis elit quis hendrerit.</p>
+    </article>
+    <article className="legalNotice__article">
+      <h2 className="legalNotice__titleArticle">Ipsum</h2>
+      <p className="legalNotice__pArticle">In ac sem eget augue efficitur auctor. Nulla mattis nisi ac eleifend blandit. Nullam non nunc orci. Fusce elementum risus ac odio vulputate, sed consectetur sapien hendrerit. Vivamus sit amet tincidunt erat. Ut at pharetra est. Aliquam pharetra diam non massa tristique dictum volutpat id elit. Sed interdum nisi iaculis elit vehicula placerat. Nam at nulla eget odio interdum consectetur at at leo. Etiam eget vestibulum lectus. Suspendisse tempor dolor malesuada quam rhoncus sagittis. Phasellus non sodales mi. Etiam mattis aliquet augue, a convallis lacus. Fusce posuere pellentesque nisl id mattis. Ut porta nisi ac augue tincidunt, in porta odio placerat.</p>
+    </article>
+    <article className="legalNotice__article">
+      <h2 className="legalNotice__titleArticle">Dolor</h2>
+      <p className="legalNotice__pArticle">Vestibulum viverra tortor vel felis tristique consequat. Phasellus vitae suscipit erat, consequat porta urna. Sed tempor nec leo ultricies posuere. Sed tincidunt tristique felis in dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam ac dolor laoreet, euismod orci at, interdum massa. Maecenas auctor elementum augue et bibendum. Sed justo nibh, auctor ac ex ut, posuere efficitur nisi. Aenean tristique leo nec nisi molestie, at varius nunc ultricies. Vivamus volutpat tincidunt tellus sed finibus. Suspendisse lacinia finibus convallis. Quisque magna elit, varius a diam non, finibus accumsan sem. Integer quis ornare turpis.</p>
+    </article>
+    <article className="legalNotice__article">
+      <h2 className="legalNotice__titleArticle">Sit amet</h2>
+      <p className="legalNotice__pArticle">Cras et pretium dolor. Sed ut ex erat. Duis laoreet sit amet tellus sed finibus. Mauris imperdiet leo turpis, vel dictum enim elementum nec. Mauris vestibulum molestie ullamcorper. Maecenas eleifend massa sed justo imperdiet dictum. Donec mollis quis nunc a sodales. Integer ac tincidunt ex. Phasellus vitae egestas tortor. Donec bibendum lacinia elit quis ultricies. Phasellus tincidunt pulvinar faucibus. Duis sodales nulla sagittis ultrices ultricies. Donec tincidunt lacus sit amet ante scelerisque malesuada.</p>
+    </article>
+    <article className="legalNotice__article">
+      <h2 className="legalNotice__titleArticle">Consectetur</h2>
+      <p className="legalNotice__pArticle">Sed urna eros, commodo non dui sit amet, scelerisque facilisis dolor. Sed tincidunt blandit tortor ac consequat. Fusce congue ante sed leo imperdiet blandit. Donec sit amet varius augue. Morbi sagittis nisi sit amet justo suscipit, nec luctus est vulputate. Suspendisse tincidunt sagittis magna non gravida. Pellentesque tempor magna vel purus luctus venenatis. Aenean eget neque pellentesque, molestie velit eget, egestas massa. Nullam placerat, velit hendrerit dignissim tincidunt, massa tellus luctus eros, id varius ligula enim non nunc. Nulla auctor mauris in sem rhoncus, laoreet ullamcorper ligula elementum. Pellentesque ornare facilisis elit, et sollicitudin felis ultrices sit amet. Integer a vulputate neque. Duis ac augue eu nulla pulvinar bibendum sit amet eu mauris.</p>
+    </article>
+    <article className="legalNotice__article">
+      <h2 className="legalNotice__titleArticle">Adipiscing</h2>
+      <p className="legalNotice__pArticle">Pellentesque vitae justo vel turpis rutrum rhoncus quis commodo augue. Praesent rhoncus vel nibh eget ultrices. Fusce mi sem, volutpat eget pharetra in, ullamcorper id felis. Donec nec placerat tellus. Proin vestibulum gravida eros efficitur pulvinar. Mauris iaculis in est at vehicula. Etiam nec efficitur lectus. Pellentesque blandit condimentum libero a aliquam. Pellentesque semper dictum cursus. Nunc tempor, nibh sed laoreet tincidunt, odio turpis pretium eros, sit amet iaculis sapien orci auctor nibh. Vivamus non neque fermentum, dapibus elit vitae, ultricies dui.</p>
+    </article>
+    <article className="legalNotice__article">
+      <h2 className="legalNotice__titleArticle">Elit</h2>
+      <p className="legalNotice__pArticle">Duis tempus accumsan ultricies. Suspendisse finibus semper dictum. Sed non magna tristique, venenatis erat non, aliquam metus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum lectus turpis, pretium sed elementum quis, fermentum quis risus. Donec sit amet volutpat metus, eu lobortis mi. Fusce massa magna, lobortis a mollis quis, pretium vitae lorem. Aenean luctus aliquet diam, quis condimentum ex lacinia ac. Nam vestibulum massa vitae dignissim mattis. Nam nec odio sem. In blandit iaculis leo eu pharetra. Nulla nec sagittis justo. Morbi leo mi, blandit vitae dignissim quis, porta ut eros. Nunc quis commodo magna, vel mollis ex. Curabitur porta felis a velit convallis commodo. Quisque fermentum metus et nisi scelerisque auctor.</p>
+    </article>
+    <article className="legalNotice__article">
+      <h2 className="legalNotice__titleArticle">Vivamus lacinia</h2>
+      <p className="legalNotice__pArticle">Quisque volutpat quis risus at congue. Duis feugiat nec metus eu rhoncus. Curabitur non tortor sit amet nulla fringilla lobortis. Vestibulum placerat et elit ac imperdiet. Curabitur quis mauris lobortis, pulvinar mi eget, dignissim nibh. Fusce enim libero, rutrum placerat sagittis in, tempor ac velit. Donec semper nisl mauris, vel ornare augue dapibus vitae. Donec aliquet lectus nisi, ut vehicula sem hendrerit a. Maecenas et sapien sit amet lectus suscipit euismod faucibus id elit. Nunc dapibus congue libero ac condimentum.</p>
+    </article>
+    <article className="legalNotice__article">
+      <h2 className="legalNotice__titleArticle">Curabitur</h2>
+      <p className="legalNotice__pArticle">Donec id lectus fermentum, volutpat libero ut, ultricies augue. Proin volutpat accumsan scelerisque. Integer faucibus dui vitae velit fermentum viverra. Vivamus fringilla imperdiet dolor, nec dapibus ipsum pretium a. Proin ultricies velit maximus elit vulputate pharetra id in ligula. Donec pharetra dolor ac lectus sagittis elementum. Morbi tincidunt sed magna ut elementum. Ut a sagittis neque. Vestibulum pharetra hendrerit elementum. In venenatis ultrices dictum. Nullam pulvinar dolor eget fringilla condimentum. Donec condimentum sapien sed nisi blandit, in tempus metus tempor. Ut tincidunt eleifend dui, sit amet elementum magna sollicitudin quis.</p>
+    </article>
+  </div>
+);
 
 export default LegalNotice;
