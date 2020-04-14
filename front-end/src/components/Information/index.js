f3b9303e91017770f2b786dd1b3e2afabc8c4475
bogdanpatroi <bujica.bogdan@gmail.com>
2020-04-14 12:11:25 +0200

    information page, html + CSS

--- front-end/src/components/Information/index.js
+++ front-end/src/components/Information/index.js
@@ -1,8 +1,85 @@
 import React from 'react';
 import './style.scss';
+import Avatar from '../../assets/images/avatar.png';
 
 const Information = () => (
-  <h1>Informationnn</h1>
+  <div className="information__container">
+    {/* Begining of the card */}
+    <div className="information__container-card-wrapper">
+      <div className="information__card">
+        <div className="information__card-image">
+          <img src={Avatar} alt="profile pic" />
+        </div>
+        <div className="information__details">
+          <h2 className="information__name">Prenom NOM
+            <br />
+            <span className="information__job-title">UI Developer</span>
+          </h2>
+        </div>
+      </div>
+    </div>
+    {/* The end of the card */}
+    {/* Begining of the card */}
+    <div className="information__container-card-wrapper">
+      <div className="information__card">
+        <div className="information__card-image">
+          <img src={Avatar} alt="profile pic" />
+        </div>
+        <div className="information__details">
+          <h2 className="information__name">Prenom NOM
+            <br />
+            <span className="information__job-title">UI Developer</span>
+          </h2>
+        </div>
+      </div>
+    </div>
+    {/* The end of the card */}
+    {/* Begining of the card */}
+    <div className="information__container-card-wrapper">
+      <div className="information__card">
+        <div className="information__card-image">
+          <img src={Avatar} alt="profile pic" />
+        </div>
+        <div className="information__details">
+          <h2 className="information__name">Prenom NOM
+            <br />
+            <span className="information__job-title">UI Developer</span>
+          </h2>
+        </div>
+      </div>
+    </div>
+    {/* The end of the card */}
+    {/* Begining of the card */}
+    <div className="information__container-card-wrapper">
+      <div className="information__card">
+        <div className="information__card-image">
+          <img src={Avatar} alt="profile pic" />
+        </div>
+        <div className="information__details">
+          <h2 className="information__name">Prenom NOM
+            <br />
+            <span className="information__job-title">UI Developer</span>
+          </h2>
+        </div>
+      </div>
+    </div>
+    {/* The end of the card */}
+    {/* Begining of the card */}
+    <div className="information__container-card-wrapper">
+      <div className="information__card">
+        <div className="information__card-image">
+          <img src={Avatar} alt="profile pic" />
+        </div>
+        <div className="information__details">
+          <h2 className="information__name">Prenom NOM
+            <br />
+            <span className="information__job-title">UI Developer</span>
+          </h2>
+        </div>
+      </div>
+    </div>
+    {/* The end of the card */}
+  </div>
 );
 
 export default Information;
