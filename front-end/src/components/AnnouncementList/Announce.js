70b4ace2ec8a2d9583f0cdc4d93499c7f7e7c23a
charlie <charlie.josuas@gmail.com>
2020-04-16 00:46:44 +0200

    update announce

--- front-end/src/components/AnnouncementList/Announce.js
+++ front-end/src/components/AnnouncementList/Announce.js
@@ -7,17 +7,19 @@ const Announce = () => (
     <div className="announce__banner" >
       <div className="announce__avatar"/>
     </div>
-    <p className="announce__user">Prénom NOM - Role</p>
+
+    <p className="announce__user">Prénom NOM, Role - <span className="announce__creation">créé le 02/54/1456</span></p>
     <div className="announce__bot">
       <div className="announce__botleft">
         <h2>Cherche quelque chose</h2>
         <p className="announce__description">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre</p>
       </div>
       <div className="announce__botright">
-        <p>du 01/02/1235 au 02/03/2314</p>
-        <p>crée le 02/54/23</p>
-        <p>Paris</p>
-        <p>bénévole</p>
+        <div className="announce__details">
+          <p>du 01/02/1235 au 02/03/2314</p>
+          <p>à Paris</p>
+          <p>Rémunérée</p>
+        </div>
         <input className="announce__button button see" type="button" value="Voir l'annonce"/>
         <input className="announce__button button seeAndEdit" type="button" value="Voir / Modifier"/>
       </div>
