b629f0cd2ec3815f8e7ac5990e105ee59cc0f3a7
Largenty <Largenty@gmail.com>
2020-04-21 22:00:04 +0200

    adding func for update an announcement

--- front-end/src/components/EditAnnouncement/index.js
+++ front-end/src/components/EditAnnouncement/index.js
@@ -1,11 +1,12 @@
 import React, {useState, Component} from 'react';
 import './style.scss';
-import DatePicker from 'react-datepicker';
-import { Editor } from 'react-draft-wysiwyg';
-import Accept from './DropZone';
 import Proptypes from 'prop-types';
 
-const EditAnnouncement = ({handleChange, title, location, description, voluntary, picture, id}) => {
+import DatePicker from 'react-datepicker';
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+const EditAnnouncement = ({handleChange, title, location, description, voluntary, picture, onEditAnnouncementSubmit, handleChangeAnnouncement, id}) => {
 
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
@@ -14,54 +15,72 @@ const EditAnnouncement = ({handleChange, title, location, description, voluntary
     <div className="editAnnouncement__container"> 
              
       <h2 className="editAnnouncement__title">Modifier votre annonce</h2>
-      <form method="get" type="submit">
+      <form onSubmit={onEditAnnouncementSubmit} method="path" >
+
         <div className="editAnnouncement__input drop desktop input" style={{backgroundImage: `url(${picture})`}}>
-        <Accept />
+          <input type="file" className="input" name="file" value="" />
         </div> 
+
         <h2 className="editAnnouncement__desktop--Title">Titre de l'annonce</h2>  
         <input onChange={handleChange} value={title} name="title" className="editAnnouncement__input title input" type="text" placeholder={title?title:"Titre de l'annonce"} />
         <p className="editAnnouncement__text editAnnouncement__desktop--Title">Date de début</p>
+
         <DatePicker className="editAnnouncement__input input"
           showPopperArrow={false}
           selected={startDate}
           dateFormat="d MMMM, yyyy"          
           onChange={date => setStartDate(date)}
         />
+
         <p className="editAnnouncement__text editAnnouncement__desktop--Title">Date de fin</p>
+
         <DatePicker className="editAnnouncement__input input"
           showPopperArrow={false}
           selected={endDate}
           dateFormat="d MMMM, yyyy"          
           onChange={date => setEndDate(date)}
         />
+
         <br/>
         <h2 className="editAnnouncement__desktop--Title">Lieu</h2>
         <input onChange={handleChange} value={location} name="location" className="editAnnouncement__input input" type="text" placeholder={location?location:"Lieu"} />
         
         <div>        
-        <input className="editAnnouncement__volunteer" type="radio" id="volonteer" name="drone" value="volonteer" defaultChecked={voluntary} />
+          <input className="editAnnouncement__volunteer" type="radio" id="volonteer" name="drone" value="volonteer" defaultChecked={voluntary} />
           <label className="editAnnouncement__volunteer">Bénévole</label>
         </div>
+
         <div>
           <input className="editAnnouncement__paid" type="radio" id="paid" name="drone" value="paid" />
           <label className="editAnnouncement__paid" defaultChecked={voluntary?false:true} >Rémunéré</label>
         </div> 
+
         <div className="editAnnouncement__textarea input">
-        <Editor onChange={handleChange} value={description} name="description"  placeholder="Description de votre projet" />
-        </div>        
+          <CKEditor
+            className="editor"
+            editor={ClassicEditor}
+            data={description}
+            onChange={handleChangeAnnouncement}
+          />
+        </div>  
+
         <div className="editAnnouncement__input mobile drop input">
-        <Accept />
-        </div>    
-      <div className="editAnnouncement__flex">
-      <button className="editAnnouncement__button button">Retour</button>
-      <button className="editAnnouncement__button button">Enregistrer</button>    
-      </div>
-    </form>
+          <input type="file" className="input" name="file" value="" />
+        </div> 
+
+        <div className="editAnnouncement__flex">
+          <button className="editAnnouncement__button button">Retour</button>
+          <button type="submit" className="editAnnouncement__button button">Enregistrer</button>    
+        </div>
+      </form>
+      
   </div>);    
 }
 ;
 
-EditAnnouncement.propTypes = {   
+EditAnnouncement.propTypes = {
+  onEditAnnouncementSubmit: PropTypes.func.isRequired,
+  handleChangeAnnouncement: PropTypes.func.isRequired,   
   title: Proptypes.string.isRequired,
   location: Proptypes.string.isRequired,
   description: Proptypes.string.isRequired,
