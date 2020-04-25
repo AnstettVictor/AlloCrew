fad16e6b3bb9e4f3bc66ec97d205b1b940d508c4
Anstettvictor <anstett.victor67@gmail.com>
2020-04-25 14:53:37 +0200

    add voter on user

--- back-end/allocrew/src/Security/Voter/UserVoter.php
+++ back-end/allocrew/src/Security/Voter/UserVoter.php
@@ -2,49 +2,55 @@
 
 namespace App\Security\Voter;
 
-use App\Entity\Announcement;
-use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
 
-class AnnouncementVoter extends Voter
+class UserVoter extends Voter
 {
     protected function supports($attribute, $subject)
     {
         // replace with your own logic
         // https://symfony.com/doc/current/security/voters.html
-        return in_array($attribute, ['ANNOUNCEMENT_EDIT', 'ANNOUNCEMENT_DELETE'])
-            && $subject instanceof \App\Entity\Announcement;
+        return in_array($attribute, ['USER_ACCOUNT_EDIT', 'USER_PASSWORD_EDIT' , 'USER_EDIT'])
+            && $subject instanceof \App\Entity\User;
     }
 
     protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
     {
         $user = $token->getUser();
-     
 
         // ... (check conditions and return true to grant permission) ...
         switch ($attribute) {
-            case 'ANNOUNCEMENT_EDIT':
-                // logic to determine if the user can EDIT
-                if ($user == $subject->getUser()) {
+            case 'USER_ACCOUNT_EDIT':
+                 // logic to determine if the user can EDIT
+                 if ($user == $subject) {
                     return true;
                 }
                     
                 if (in_array('ROLE_ADMIN', $user->getRoles())) {
                     return true;
                 }
-               
                 break;
-            case 'ANNOUNCEMENT_DELETE':
-                // logic to determine if the user can DELETE
-                if ($user == $subject->getUser()) {
+            case 'USER_PASSWORD_EDIT':
+                // logic to determine if the user can EDIT
+                if ($user == $subject) {
                     return true;
                 }
                     
                 if (in_array('ROLE_ADMIN', $user->getRoles())) {
                     return true;
                 }
-                
+                break;
+            case 'USER_EDIT':
+                // logic to determine if the user can EDIT
+                if ($user == $subject) {
+                    return true;
+                }
+                        
+                if (in_array('ROLE_ADMIN', $user->getRoles())) {
+                    return true;
+                }
                 break;
         }
 
