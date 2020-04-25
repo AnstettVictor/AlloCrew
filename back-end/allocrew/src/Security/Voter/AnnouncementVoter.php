6653ad968596cd985f733e4ff076c8d5eab2ba35
Anstettvictor <anstett.victor67@gmail.com>
2020-04-25 14:24:08 +0200

    add voter on announcement

---
+++ back-end/allocrew/src/Security/Voter/AnnouncementVoter.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Announcement;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+class AnnouncementVoter extends Voter
+{
+    protected function supports($attribute, $subject)
+    {
+        // replace with your own logic
+        // https://symfony.com/doc/current/security/voters.html
+        return in_array($attribute, ['ANNOUNCEMENT_EDIT', 'ANNOUNCEMENT_DELETE'])
+            && $subject instanceof \App\Entity\Announcement;
+    }
+
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        $user = $token->getUser();
+        // if the user is anonymous, do not grant access
+        // if (!$user instanceof UserInterface) {
+        //     return false;
+        // }
+
+        // ... (check conditions and return true to grant permission) ...
+        switch ($attribute) {
+            case 'ANNOUNCEMENT_EDIT':
+                // logic to determine if the user can EDIT
+                if ($user == $subject->getUser()) {
+                    return true;
+                }
+                    
+                if (in_array('ROLE_ADMIN', $user->getRoles())) {
+                    return true;
+                }
+               
+                break;
+            case 'ANNOUNCEMENT_DELETE':
+                // logic to determine if the user can DELETE
+                if ($user == $subject->getUser()) {
+                    return true;
+                }
+                    
+                if (in_array('ROLE_ADMIN', $user->getRoles())) {
+                    return true;
+                }
+                
+                break;
+        }
+
+        return false;
+    }
+}
