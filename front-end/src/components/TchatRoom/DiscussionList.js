df344c3d0b07493f40463410f7432f6302161ebb
Largenty <Largenty@gmail.com>
2020-04-26 21:45:30 +0200

    small change in css

--- front-end/src/components/TchatRoom/DiscussionList.js
+++ front-end/src/components/TchatRoom/DiscussionList.js
@@ -5,11 +5,11 @@ import { Link } from "react-router-dom";
 
 const DiscussionList = ({by_creator, by_receiver, message, handleMessage, onMessageSubmit,userId}) => (
   <> 
-    <h3>DISCUTION CREE</h3>
+    <h3>DISCUTION RECU</h3>
     { console.log("list",{by_creator}),
       by_creator.map((discussion) =>
       <div key={discussion.id}>
-      <h3 className="discussion__spaceText"><Link to={`/profile/${discussion.creator.id}`}>
+      <h3 className="discussion__spaceText"><Link to={`/profile/${discussion.receiver.id}`}>
         <span>{discussion.receiver.firstname}</span>
         <span className=""> {discussion.receiver.lastname}</span> </Link></h3> 
         <h4> {discussion.announcement.title}</h4>
@@ -33,7 +33,7 @@ const DiscussionList = ({by_creator, by_receiver, message, handleMessage, onMess
      </div>)
     }
 
-<h3>DISCUTION RECU</h3>
+<h3>DISCUTION CREE</h3>
     { console.log("list",{by_creator}),
       by_receiver.map((discussion) =>
       <div key={discussion.id}>
