02fb05199c0faea31a373177d900a6278ed691d8
Largenty <Largenty@gmail.com>
2020-04-26 17:42:55 +0200

    adding message

--- front-end/src/components/TchatRoom/DiscussionList.js
+++ front-end/src/components/TchatRoom/DiscussionList.js
@@ -13,19 +13,39 @@ const DiscussionList = ({by_creator, by_receiver, userId}) => (
         <span>{discussion.receiver.firstname}</span>
         <span className=""> {discussion.receiver.lastname}</span> </Link></h3> 
         <h4> {discussion.announcement.title}</h4>
-      <button  className="discussion__button"  >+</button> 
-    </div>)
+      <button  className="discussion__button"  >+</button>
+      {
+        discussion.messages.map((message) =>  
+        <div key={message.id} className="message__container">
+          <div > 
+            <h4 className="">{message.user.firstname} : </h4>               
+            <p>{message.content}</p>
+          </div>
+        </div>
+      )
+      }
+     </div>)
     }
 
 <h3>DISCUTION RECU</h3>
     { console.log("list",{by_creator}),
       by_receiver.map((discussion) =>
       <div >
-      <h3 className="discussion__spaceText"><Link to={`/profile/${discussion.creator.id}`}>
-        <span>{discussion.creator.firstname}</span>
-        <span className=""> {discussion.creator.lastname}</span> </Link></h3> 
-        <h4> {discussion.announcement.title}</h4>
-      <button  className="discussion__button"  >+</button> 
+        <h3 className="discussion__spaceText"><Link to={`/profile/${discussion.creator.id}`}>
+          <span>{discussion.creator.firstname}</span>
+          <span className=""> {discussion.creator.lastname}</span> </Link></h3> 
+          <h4> {discussion.announcement.title}</h4>
+        <button  className="discussion__button"  >+</button> 
+      {
+        discussion.messages.map((message) =>  
+        <div key={message.id} className="message__container">
+          <div > 
+            <h4 className="">{message.user.firstname} : </h4>               
+            <p>{message.content}</p>
+          </div>
+        </div>
+      )
+      }
     </div>)
     }
   </>
