dfc68c5dd1003e1fedadb14da9795708e7ae9087
charlie <charlie.josuas@gmail.com>
2020-04-30 01:45:39 +0200

    edit profile style

--- front-end/src/components/Announcement/index.js
+++ front-end/src/components/Announcement/index.js
@@ -1,68 +1,68 @@
 import React from 'react';
 import './style.scss';
 import Proptypes from 'prop-types'
-import {Link, Redirect} from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import ReactHtmlParser from 'react-html-parser';
-import {date} from 'utils/functions';
+import { date } from 'utils/functions';
 import arrow from 'images/svg/arrow.svg';
 
-const Announcement = ({title, location, description, picture, voluntary, id, dateEnd, dateStart, active, user, sendingMessage, createdAt, userId, redirect }) => {
+const Announcement = ({ title, location, description, picture, voluntary, id, dateEnd, dateStart, active, user, sendingMessage, createdAt, userId, redirect }) => {
 
 
-if(redirect == 'tchat'){
-  return <Redirect to={`/tchat-room/${userId}`} />
-}
-return (
-  <div className="announcement__container" >
-   
-    <div className="announcement__img" style={{backgroundImage: `url(${picture})`}}/>  
-  
-    <div className="announcement__pos1">       
+  if (redirect == 'tchat') {
+    return <Redirect to={`/tchat-room/${userId}`} />
+  }
+  return (
+    <div className="announcement__container" >
 
-      <h2 className="announcement__title">{title}</h2>
-      <div className="announcement__textInfo">
+      <div className="announcement__img" style={{ backgroundImage: `url(${picture})` }} />
 
-        <p className="announcement__place">Publiée le {date(createdAt)}</p>
-        <p className="announcement__author">par : {user.firstname} {user.lastname}</p>
-        <p className={voluntary? "announcement__volunteer": "announcement__paid"}>
-          Annonce <span>{voluntary? "bénévole": "rémunérée"}</span>
-        </p>
-      </div>
+      <div className="announcement__pos1">
 
-      <div className="announcement__textInfo-2">
-        <p className="announcement__date">Du {date(dateStart)} au {date(dateEnd)} à {location}</p>
-        <p className="announcement__description">Description:</p>
-        <div className="announcement__text">{ReactHtmlParser(description)}</div>            
-      </div>
-    
-   
+        <h2 className="announcement__title">{title}</h2>
+        <div className="announcement__textInfo">
+
+          <p className="announcement__place">Publiée le {date(createdAt)}</p>
+          <p className="announcement__author">par : {user.firstname} {user.lastname}</p>
+          <p className={voluntary ? "announcement__volunteer" : "announcement__paid"}>
+            Annonce <span>{voluntary ? "bénévole" : "rémunérée"}</span>
+          </p>
+        </div>
+
+        <div className="announcement__textInfo-2">
+          <p className="announcement__date">Du {date(dateStart)} au {date(dateEnd)} à {location}</p>
+          <p className="announcement__description">Description:</p>
+          <div className="announcement__text">{ReactHtmlParser(description)}</div>
+        </div>
 
 
-      {/* Affichage conditionnel du bouton */}
-      {
-        userId != user.id && (
-          
-          <form onSubmit={(e) => {e.preventDefault(); sendingMessage({user_id: user.id, announcement_id: id})}} method="post">            
-            <button className="button announcement__conditionButton" type="submit" name={user.id}>Envoyer un message</button>
-          </form>
-          
-        )
-      }
+
+
+        {/* Affichage conditionnel du bouton */}
+        {
+          userId != user.id && (
+
+            <form onSubmit={(e) => { e.preventDefault(); sendingMessage({ user_id: user.id, announcement_id: id }) }} method="post">
+              <button className="button announcement__conditionButton" type="submit" name={user.id}>Envoyer un message</button>
+            </form>
+
+          )
+        }
         {userId == user.id && (<Link to={`/edit-announcement/${id}`}>
-          <button className="button announcement__conditionButton "  name={user.id}>Modifier</button>
+          <button className="button announcement__conditionButton " name={user.id}>Modifier</button>
         </Link>)
-      }
-      <Link className="back__wrapper" to="/home">
-        <div className="announcement__back">Retour</div>
-      </Link>
-      <br/>
-      <img className="arrow" src={arrow}/>
+        }
+        <Link className="back__wrapper" to="/home">
+          <div className="announcement__back">Retour</div>
+        </Link>
+        <br />
+        <img className="arrow" src={arrow} />
+      </div>
     </div>
-  </div>
-)
+  )
 };
 
-Announcement.propTypes = {   
+Announcement.propTypes = {
   title: Proptypes.string.isRequired,
   location: Proptypes.string.isRequired,
   description: Proptypes.string.isRequired,
@@ -70,7 +70,7 @@ Announcement.propTypes = {
   voluntary: Proptypes.bool.isRequired,
   id: Proptypes.number.isRequired,
   dateStart: Proptypes.string.isRequired,
-  dateEnd: Proptypes.string.isRequired, 
+  dateEnd: Proptypes.string.isRequired,
   active: Proptypes.bool.isRequired,
   fetchData: Proptypes.func,
   createdAt: Proptypes.number.isRequired,
@@ -79,7 +79,7 @@ Announcement.propTypes = {
     firstname: Proptypes.string.isRequired,
     lastname: Proptypes.string.isRequired,
     picture: Proptypes.string.isRequired,
-  })      
+  })
 }
 
 
--- front-end/src/components/EditProfile/index.js
+++ front-end/src/components/EditProfile/index.js
@@ -1,116 +1,113 @@
 import React from 'react';
-import {Link, Redirect} from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import './style.scss';
 import PropTypes from 'prop-types';
 import CKEditor from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 
-const EditProfile = ({id, onEditProfileSubmit, title, firstname, lastname, age, location, description, experience, portfolio, bannerpicture, picture, handleChange, handleChangeEditor1, handleChangeEditor2, redirect, appendImage, notification, uploadImage, uploadAvatar, appendAvatar }) => {
+const EditProfile = ({ id, onEditProfileSubmit, title, firstname, lastname, age, location, description, experience, portfolio, bannerpicture, picture, handleChange, handleChangeEditor1, handleChangeEditor2, redirect, appendImage, notification, uploadImage, uploadAvatar, appendAvatar }) => {
 
-  if(redirect) {
+  if (redirect) {
     return <Redirect to="/profile" />
   }
-return  (
-  <div className="editProfile__container">
-    <form onSubmit={onEditProfileSubmit} method="patch" >
-      <div className="editProfile__container">    
-        <div className="editProfile__pict">
-        <button type="submit"  className="editProfile__button button">Enregistrer</button>
-          <Link to={`/profile/${id}`}>
-            <button className="editProfile__buttonRetour button">Retour</button>
-          </Link>          
+  return (
+    <div className="editProfile__container">
+      <form onSubmit={onEditProfileSubmit} method="patch" >
+        <div className="editProfile__container">
+          <div className="editProfile__pict">
 
-          <div className="input editProfile__pict__cover" style={{backgroundImage: `url(${bannerpicture})`}}>
-          <input
-            type="file"
-            className="input"
-            name="bannerpicture"
-            onChange={appendImage}
-          />
-          <div onClick={uploadImage} className="button">Importer</div>
-          {notification && <strong>{notification}</strong>}
-          <p>Image de l'annonce</p>
-          </div>
+            <div className="input editProfile__pict__cover" style={{ backgroundImage: `url(${bannerpicture})` }}>
+              {notification && <div className="notification">{notification}</div>}
+              <input
+                type="file"
+                className="input"
+                name="bannerpicture"
+                onChange={appendImage}
+              />
+              <div onClick={uploadImage} className="button">Importer</div>
+            </div>
 
-          <div className="editProfile__pict__editProfile" style={{backgroundImage: `url(${picture})`}}>
-          <input
-            type="file"
-            className="input"
-            name="picture"
-            onChange={appendAvatar}
-          />
-            <div onClick={uploadAvatar} className="button">Importer</div>
-          {notification && <strong>{notification}</strong>}
-          <p>Image de l'annonce</p>
+            <div className="editProfile__pict__editProfile" style={{ backgroundImage: `url(${picture})` }}>
+            </div>
+            <button type="submit" className="editProfile__button button">Enregistrer</button>
+            <Link to={`/profile/${id}`}>
+              <button className="editProfile__buttonRetour button">Retour</button>
+            </Link>
+              <input
+                type="file"
+                className="input avatar__input"
+                name="picture"
+                onChange={appendAvatar}
+              />
+              <div onClick={uploadAvatar} className="import__avatar button">Importer</div>
+            </div>
           </div>
-        </div>
-        <div className="editProfile__mainInfos"> 
-          <div>
-            <label>Nom</label>
-            <input onChange={handleChange} value={lastname?lastname:""} name="lastname" type="text" className=" input" placeholder={lastname?lastname :"Nom"} required/>
-          </div>
-          
-          <div>
-            <label>Prénom</label>
-            <input onChange={handleChange} value={firstname?firstname:""} name="firstname" type="text" className=" input" placeholder={firstname?firstname :"Prénom"} required/>
-          </div>
+          <div className="editProfile__mainInfos">
+            <div>
+              <label>Nom</label>
+              <input onChange={handleChange} value={lastname ? lastname : ""} name="lastname" type="text" className=" input" placeholder={lastname ? lastname : "Nom"} required />
+            </div>
 
-          <div>
-            <label>Age</label>
-            <input onChange={handleChange} value={age?age:""} name="age" type="number" className=" input" placeholder={age?age:"Votre âge"}/>
+            <div>
+              <label>Prénom</label>
+              <input onChange={handleChange} value={firstname ? firstname : ""} name="firstname" type="text" className=" input" placeholder={firstname ? firstname : "Prénom"} required />
+            </div>
+
+            <div>
+              <label>Age</label>
+              <input onChange={handleChange} value={age ? age : ""} name="age" type="number" className=" input" placeholder={age ? age : "Votre âge"} />
+            </div>
+
+            <div>
+              <label>Ville</label>
+              <input onChange={handleChange} value={location ? location : ""} name="location" type="text" className=" input" placeholder={location ? location : "Votre ville"} required />
+            </div>
+
+
+            <div>
+              <label>Profession</label>
+              <input onChange={handleChange} value={title ? title : ""} name="title" type="text" className=" input" placeholder={title ? title : "Votre profession"} />
+            </div>
+
           </div>
-        
-          <div>
-            <label>Ville</label>
-            <input onChange={handleChange} value={location?location:""} name="location" type="text" className=" input" placeholder={location?location :"Votre ville"} required/>
-          </div>
-        
-          
-          <div>
-            <label>Profession</label>
-            <input onChange={handleChange} value={title?title:""} name="title" type="text" className=" input" placeholder={title?title :"Votre profession"}/>
-          </div>
-                    
-        </div>
-        <div className="editProfile__textArea">
-          <div className="description editor input ">
-            <label className="">Description</label>
-            <CKEditor
-              className="editor"
-              editor={ClassicEditor}
-              data={description}
-              onChange={handleChangeEditor1}
-              config={{
-                removePlugins: [ 'EasyImage', 'Image', 'ImageCaption', 'ImageStyle', 'ImageToolbar', 'ImageUpload', 'MediaEmbed', 'TableToolbar', 'Table', 'Indent' ],
-              }}
-            />
-          </div>
-        
-          <div className="experience editor input ">
-            <label className="">Expérience</label>
-            <CKEditor
+          <div className="editProfile__textArea">
+            <div className="description editor input ">
+              <label className="">Description</label>
+              <CKEditor
                 className="editor"
-              editor={ClassicEditor}
-              data={experience}
-              onChange={handleChangeEditor2}
-              config={{
-                removePlugins: [ 'EasyImage', 'Image', 'ImageCaption', 'ImageStyle', 'ImageToolbar', 'ImageUpload', 'MediaEmbed', 'TableToolbar', 'Table', 'Indent' ],
-              }}
-            />
-          </div>
-          <div>
-            <label className="">Portfolio:</label>
-            <input onChange={handleChange} name="portfolio" type="text" className="editProfile__link input"  value={portfolio?portfolio:""} placeholder={portfolio?portfolio :"Lien vers votre book, votre bande-démo, etc..."}/>     
+                editor={ClassicEditor}
+                data={description}
+                onChange={handleChangeEditor1}
+                config={{
+                  removePlugins: ['EasyImage', 'Image', 'ImageCaption', 'ImageStyle', 'ImageToolbar', 'ImageUpload', 'MediaEmbed', 'TableToolbar', 'Table', 'Indent'],
+                }}
+              />
+            </div>
+
+            <div className="experience editor input ">
+              <label className="">Expérience</label>
+              <CKEditor
+                className="editor"
+                editor={ClassicEditor}
+                data={experience}
+                onChange={handleChangeEditor2}
+                config={{
+                  removePlugins: ['EasyImage', 'Image', 'ImageCaption', 'ImageStyle', 'ImageToolbar', 'ImageUpload', 'MediaEmbed', 'TableToolbar', 'Table', 'Indent'],
+                }}
+              />
+            </div>
+            <div>
+              <label className="">Portfolio:</label>
+              <input onChange={handleChange} name="portfolio" type="text" className="editProfile__link input" value={portfolio ? portfolio : ""} placeholder={portfolio ? portfolio : "Lien vers votre book, votre bande-démo, etc..."} />
           </div>
         </div>
-      </div>
-    </form>
-  </div>
-  
-)
+      </form>
+    </div>
+
+  )
 }
-;
+  ;
 
 // EditProfile.propTypes = {
 //   onEditProfileSubmit: PropTypes.func.isRequired,
